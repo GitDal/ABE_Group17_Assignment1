d0be350da3608967a654d058eebc22a556ab0a5b
GitDal <jeppedybdal@gmail.com>
2021-02-26 12:19:55 +0100

    Strongly typed controller functions

--- src/controllers/indexController.ts
+++ src/controllers/indexController.ts
@@ -1,3 +1,5 @@
-export async function index (req, res, next) {
+import express from "express"
+
+export async function index(req: express.Request , res: express.Response, next: express.NextFunction) {
     res.status(200).send("HEJ HEJ");
 }
