e76a7a3c07eb20a64810b37ee43e245d29896baf
Jeppe Dybdal Larsen <au616966@uni.au.dk>
2021-03-03 19:48:39 +0100

    'Heroku'

--- src/models/db.ts
+++ src/models/db.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 //const url = 'mongodb://localhost:27017/ABE';
 // For heroku deployment:
-const url = "mongodb+srv://admin:admin123@abe-assignment1-databas.rabnd.mongodb.net/ABE?retryWrites=true&w=majority";
+const url = process.env.DB_CONNECTION_STRING as string;
 
 async function connect() {
     try {
